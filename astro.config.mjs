9ef3d0356cdd2bb87f55c12877d8f5a47f63adba
SpoogyOH <opiola.daniel@gmail.com>
2025-01-15 22:42:34 +0100

    test hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [ react(), tailwind()],
-  output: "server",
-  adapter: vercel()
+  output: "hybrid",
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
